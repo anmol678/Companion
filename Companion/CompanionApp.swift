5b2d32939116e18b00a89644b94f18858a4fe6ae
Anmol Jain <anmolj678@gmail.com>
2022-10-09 18:58:49 -0700

    Initial Commit

---
+++ Companion/CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompanionApp.swift
+//  Companion
+//
+//  Created by Anmol Jain on 10/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Companion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Companion
+//
+//  Created by Anmol Jain on 10/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
